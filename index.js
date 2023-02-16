0d4091b97a109e43bc8599f9448b3b28ac7d58e9
Ankit5256 <83938383+Ankit5256@users.noreply.github.com>
2023-02-16 11:04:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,40 +37,43 @@ for(var i=0;i<numberOfDrumButtons;i++){
     }
   });
 }
+$(document).keypress(function(event){
+ buttonAnimation(event.key);
+ switch (event.key) {
+  case "w":
+      var tm1= new Audio("sounds/tom-1.mp3");
+      tm1.play();
+    break;
+  case "a":
+      var tm2= new Audio("sounds/tom-2.mp3");
+      tm2.play();
+    break;
+  case "s":
+      var tm3= new Audio("sounds/tom-3.mp3");
+      tm3.play();
+    break;
+  case "d":
+      var tm4= new Audio("sounds/tom-4.mp3");
+      tm4.play();
+    break;
+  case "j":
+      var snar= new Audio("sounds/snare.mp3");
+      snar.play();
+    break;
+  case "k":
+      var cras= new Audio("sounds/crash.mp3");
+      cras.play();
+    break;
+  case "l":
+      var kick_bas= new Audio("sounds/kick-bass.mp3");
+      kick_bas.play();
+    break;
 
-//  document.addEventListener("keypress", function (event){
-//
-//   switch (event.key) {
-//     case "w":
-//         var tom1= new Audio("sounds/tom-1.mp3");
-//         tom1.play();
-//       break;
-//     case "a":
-//         var tom2= new Audio("sounds/tom-2.mp3");
-//         tom2.play();
-//       break;
-//     case "s":
-//         var tom3= new Audio("sounds/tom-3.mp3");
-//         tom3.play();
-//       break;
-//     case "d":
-//         var tom4= new Audio("sounds/tom-4.mp3");
-//         tom4.play();
-//       break;
-//     case "j":
-//         var snare= new Audio("sounds/snare.mp3");
-//         snare.play();
-//       break;
-//     case "k":
-//         var crash= new Audio("sounds/crash.mp3");
-//         crash.play();
-//       break;
-//     case "l":
-//         var kick_bass= new Audio("sounds/kick-bass.mp3");
-//         kick_bass.play();
-//       break;
-//
-// });
+
+}
+});
+
+
 function buttonAnimation(currkey){
  var activeButton = document.querySelector("."+currkey);
  activeButton.classList.add("pressed");
